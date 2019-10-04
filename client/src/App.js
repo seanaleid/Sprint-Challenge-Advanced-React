8b544825a6683d472de848cea88b8a10fee5ba10
seanaleid <seanaleid@gmail.com>
2019-10-04 11:43:35 -0400

    Step 1: createa class component that fetches data from the API call --> localhost:5000

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,39 @@
 import React from 'react';
 import './App.css';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  //create class component and state
+  state={
+    player: [],
+  }
+
+  componentDidMount() {
+    //add axios to call
+    //run npm i on the root folder to start localhost: 5000
+    axios
+      .get(`http://localhost:5000/api/players`)
+      .then(res =>{
+        //console.log res, and continue until desired data is found
+        console.log(`the response form the axios call is:`, res.data)
+        //console.log typeof res.data
+        console.log(`res.data is a/an`, typeof res.data)
+        //set res.data to state
+        this.setState({
+          player: res.data
+        })
+      })
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Test, hello world!</h1>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
